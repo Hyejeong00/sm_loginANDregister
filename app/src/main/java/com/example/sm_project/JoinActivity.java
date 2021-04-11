b6fd7135527fde285d1b8413a817ab00de8416fe
Hyejeong00 <love72771112@gmail.com>
2021-04-12 00:53:40 +0900

    Login and Register

---
+++ app/src/main/java/com/example/sm_project/JoinActivity.java
@@ -0,0 +1,155 @@
+package com.example.sm_project;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class JoinActivity extends AppCompatActivity {
+    private EditText et_id, et_pass, et_pass_check, et_name, et_email;
+    private Button btn_check, btn_register;
+    private AlertDialog dialog;
+    private boolean validate = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+
+
+        et_id = findViewById(R.id.et_id);
+        et_pass = findViewById(R.id.et_pass);
+        et_name = findViewById(R.id.et_name);
+        et_email = findViewById(R.id.et_email);
+        et_pass_check = findViewById(R.id.et_pass_check);
+
+        //아이디 중복 체크
+        btn_check = findViewById(R.id.btn_check);
+        btn_check.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String userID = et_id.getText().toString();
+                if (validate) {
+                    return; //검증 완료
+                }
+
+                if (userID.equals("")) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(JoinActivity.this);
+                    dialog = builder.setMessage("아이디를 입력하세요.").setPositiveButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if (success) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(JoinActivity.this);
+                                dialog = builder.setMessage("사용할 수 있는 아이디입니다.").setPositiveButton("확인", null).create();
+                                dialog.show();
+                                et_id.setEnabled(false); //아이디값 고정
+                                validate = true; //검증 완료
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(JoinActivity.this);
+                                dialog = builder.setMessage("이미 존재하는 아이디입니다.").setNegativeButton("확인", null).create();
+                                dialog.show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                ValidateRequest validateRequest = new ValidateRequest(userID, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(JoinActivity.this);
+                queue.add(validateRequest);
+            }
+        });
+
+        //회원가입 버튼 클릭 시 수행
+        btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userID = et_id.getText().toString();
+                String userPass = et_pass.getText().toString();
+                String userPass_Check = et_pass_check.getText().toString();
+                String userName = et_id.getText().toString();
+                String userEmail = et_email.getText().toString();
+
+                //아이디 중복체크 했는지 확인
+                if (!validate) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(JoinActivity.this);
+                    dialog = builder.setMessage("중복된 아이디가 있는지 확인하세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+
+                //한 칸이라도 입력 안했을 경우
+                if (userEmail.equals("") || userPass.equals("") || userPass_Check.equals("") || userName.equals("") || userEmail.equals("")) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(JoinActivity.this);
+                    dialog = builder.setMessage("모두 입력해주세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+
+                            //회워가입 성공시
+                            if (userPass.equals(userPass_Check)) {
+                                if (success) {
+                                    Toast.makeText(getApplicationContext(), "회원등록에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(JoinActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+
+                                    //회원가입 실패시
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "회원등록에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(JoinActivity.this);
+                                dialog = builder.setMessage("비밀번호가 동일하지 않습니다.").setNegativeButton("확인", null).create();
+                                dialog.show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                //서버로 Volley를 이용해서 요청
+                JoinRequest joinRequest = new JoinRequest(userID,userPass,userName,userEmail,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(JoinActivity.this);
+                queue.add(joinRequest);
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/sm_project/JoinRequest.java
@@ -0,0 +1,27 @@
+package com.example.sm_project;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JoinRequest extends StringRequest {
+    final static private String URL = "http://dlgpwjd1112.dothome.co.kr/Register.php";
+    private Map<String, String> map;
+
+    public JoinRequest(String userID, String userPassword, String userName, String userEmail, Response.Listener<String> listener) {
+        super(Method.POST,URL,listener,null);
+        map = new HashMap<>();
+        map.put("userID",userID);
+        map.put("userPassword",userPassword);
+        map.put("userName", userName);
+        map.put("userEmail", userEmail);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
